cec1533ec49357fc0293af71acb2792e4d951f81
Ty Miller <ty.loves.code@gmail.com>
2019-02-12 23:17:13 -0500

    Edited header

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: https://tymiller.net/pt/success.html');
+    header('Location: http://tymiller.net/pt/success.html');
   }
