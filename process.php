d1bead0bbe01e4a4c61c89ef217d7a17af5c1fb5
Ty Miller <ty.loves.code@gmail.com>
2019-02-12 23:15:44 -0500

    Updated header info

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: https://tylovescode.github.io/paytons-tees/success.html');
+    header('Location: https://tymiller.net/pt/success.html');
   }
