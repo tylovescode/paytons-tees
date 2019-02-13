33419696e74ef94a78860d4e5cf2620ab0721edf
Ty Miller <ty.loves.code@gmail.com>
2019-02-12 23:03:29 -0500

    Edited php file

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
+    header('Location: https://tylovescode.github.io/paytons-tees/success.html');
   }
